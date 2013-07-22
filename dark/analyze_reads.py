8bc1a7e73628c4a243fb7ebc8f00b54f8c07ccb2
Barbara Muehlemann <barbara.muhlemann@bluewin.ch>
2013-07-22 15:13:13 +0100

    added analyze_reads in dark and run_analyze_reads in bin

---
+++ dark/analyze_reads.py
@@ -0,0 +1,14 @@
+from turn_reads_nr3 import getReads, _longestPrefixOfTwoSeqs
+from Bio import SeqIO
+import sys
+
+
+if len(sys.argv) > 2:
+    print >> stderr, "ERROR, takes at least two arguments."
+    sys.exit(1)
+
+else:
+    caller = sys.argv[1]
+    getReads(caller)
+
+
