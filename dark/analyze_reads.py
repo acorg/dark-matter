6ef75d64934cf2e51d2aed2230cc04d87a9fea3b
Barbara Muehlemann <barbara.muhlemann@bluewin.ch>
2013-07-22 15:26:12 +0100

    added new analyze_reads, the previous one was the wrong file

--- dark/analyze_reads.py
+++ dark/analyze_reads.py
@@ -1,14 +1,59 @@
-from turn_reads_nr3 import getReads, _longestPrefixOfTwoSeqs
 from Bio import SeqIO
-import sys
 
 
-if len(sys.argv) > 2:
-    print >> stderr, "ERROR, takes at least two arguments."
-    sys.exit(1)
+def _longestPrefixOfTwoSeqs(a, b):
+    length = min(len(a), len(b))
+    result = 0
+    while result < length:
+        if a[result] == b[result]:
+            result += 1
+        else:
+            break
+    return result
 
-else:
-    caller = sys.argv[1]
-    getReads(caller)
 
+def getReads(filename):
+    read_list = []
+    for record in SeqIO.parse(open(filename, "rU"), "fasta"):
+        read_list.append(record)
+    print read_list
+
+    reversed_read_list = map(lambda read: read[::-1], read_list)
+    print reversed_read_list
+
+    def longestCommonPrefix(read_list):
+        sequences = read_list
+        nSequences = len(sequences)
+        if nSequences == 1:
+            print "This file contains one read only!"
+            return len(sequences[0])
+        else:
+            prefix = sequences[0]
+            result = len(prefix)
+            index = 1
+            while index < nSequences:
+                thisLen = _longestPrefixOfTwoSeqs(prefix, sequences[index])
+                if thisLen == 0:
+                    result = 0
+                    return result
+                elif thisLen < result:
+                    prefix = prefix[:thisLen]
+                    result = thisLen
+                    return result
+                index += 1
+
+    result_prefix = longestCommonPrefix(read_list)
+    result_suffix = longestCommonPrefix(reversed_read_list)
+    print "result_prefix", result_prefix
+    print "result_suffix", result_suffix
+
+    def clipSequences(filename):
+        print result_prefix
+        print result_suffix
+        for record in SeqIO.parse(open(filename, "rU"), "fasta"):
+            if result_suffix == 0:
+                print record[result_prefix:]
+            else:
+                print record[result_prefix:-result_suffix]
+    clipSequences(filename)
 
