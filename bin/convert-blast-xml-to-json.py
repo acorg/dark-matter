e3bd6d4b1d4fd4d7bf36911d4e58c57908dffd41
terrycojones <terry@fluidinfo.com>
2013-08-24 23:22:10 +0100

    Adjust bin/convert-blast-xml-to-json.py so it can take an output file on the command line for running with HTCondor.

--- bin/convert-blast-xml-to-json.py
+++ bin/convert-blast-xml-to-json.py
@@ -3,10 +3,17 @@
 from dark.conversion import convertBlastXMLToJSON
 
 
+def _convert(xmlfile, outfp):
+    convertBlastXMLToJSON(xmlfile, outfp)
+
+
 if __name__ == '__main__':
     import sys
-    if len(sys.argv) != 2:
-        print >>sys.stderr, 'Usage: %s infile.xml' % sys.argv[0]
-        sys.exit(1)
+    if len(sys.argv) == 2:
+        _convert(sys.argv[1], sys.stdout)
+    if len(sys.argv) == 3:
+        with open(sys.argv[2], 'w') as fp:
+            _convert(sys.argv[1], fp)
     else:
-        convertBlastXMLToJSON(sys.argv[1], sys.stdout)
+        print >>sys.stderr, 'Usage: %s infile.xml [outfile.json]' % sys.argv[0]
+        sys.exit(1)
