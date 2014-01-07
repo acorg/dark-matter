improved idList, fixed minE, eMean and eMedian calculation

dark/blast.py
@@ -3,6 +3,7 @@ import numpy as np
 from random import uniform
 from Bio.Blast import NCBIXML
 from Bio import SeqIO
+import numpy as np
 
 from dark.conversion import readJSONRecords
 from dark.filter import HitInfoFilter, TitleFilter
@@ -496,10 +497,13 @@ class BlastHits(object):
                 'hspTotal': 0,  # The total number of HSPs for this title.
                 'items': [],  # len() of this = the # of HSPs kept for display.
                 'maxE': None,
-                'minE': None,
+                'minE': 1000,  # set to large number, so it can be reduced
                 'maxX': maxStop or sequenceLen,
                 'minX': minStart or 0,
                 'zeroEValueFound': False,
+                'originalEValues': [],
+                'eMean': None,
+                'eMedian': None,
             }
 
             if logLinearXAxis:
@@ -546,6 +550,10 @@ class BlastHits(object):
                 e = hsp.expect
                 if eCutoff is not None and e >= eCutoff:
                     continue
+
+                # retain original evalue below cutoff for eMean and eMedian
+                # calculation.
+                plotInfo['originalEValues'].append(e)
 
                 if e == 0.0:
                     convertedE = None
@@ -644,3 +652,8 @@ class BlastHits(object):
                     plotInfo['maxX'] = maxX
 
                 plotInfo['offsetAdjuster'] = adjuster
+
+            # calculate eMedian and eMean
+            plotInfo['eMean'] = np.mean(plotInfo['originalEValues'])
+            plotInfo['eMedian'] = np.median(plotInfo['originalEValues'])
+            del plotInfo['originalEValues']

dark/graphics.py
@@ -5,7 +5,6 @@ from collections import defaultdict
 from time import ctime, time
 from math import log10
 import matplotlib.pyplot as plt
-import numpy as np
 from matplotlib.lines import Line2D
 from matplotlib.patches import Rectangle
 from matplotlib import gridspec
@@ -297,19 +296,18 @@ def alignmentGraph(blastHits, title, addQueryLines=True, showFeatures=True,
                           color='blue')
             readsAx.add_line(line)
 
-        # TODO: Barbara, add some kind of comment here please. I think this
-        # could be done more efficiently.
+        # if an idList is given, color reads accordingly
         if idList:
             for item in items:
-                for key in idList:
-                    for ids in idList[key]:
-                        if ids == item['query']:
-                            e = item['convertedE']
-                            hsp = item['hsp']
-                            line = Line2D([hsp['subjectStart'],
-                                           hsp['subjectEnd']], [e, e],
-                                          color=key)
-                            readsAx.add_line(line)
+                queryId = blastHits.fasta[item['readNum']].id
+                for key, values in idList.iteritems():
+                    if queryId in values:
+                        e = item['convertedE']
+                        hsp = item['hsp']
+                        line = Line2D([hsp['subjectStart'],
+                                       hsp['subjectEnd']], [e, e],
+                                      color=key)
+                        readsAx.add_line(line)
 
     # Add to ORF figures and add vertical lines for the sequence features.
     # The feature and ORF display are linked and they shouldn't be. This
@@ -493,9 +491,8 @@ def alignmentPanel(blastHits, sortOn='eMedian', interactive=True,
             if blastHits.plotParams['rankEValues']:
                 plotTitle += '\ne-values are ranks'
             else:
-                eValues = [item['convertedE'] for item in plotInfo['items']]
-                median = np.median(eValues)
-                mean = np.mean(eValues)
+                median = plotInfo['eMedian']
+                mean = plotInfo['eMean']
                 plotTitle += '\nmedian 1e-%d, mean 1e-%d' % (median, mean)
 
         ax[row][col].set_title(plotTitle, fontsize=10)