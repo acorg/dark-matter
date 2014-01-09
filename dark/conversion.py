ec139ac7e7b555da97248f7a376495a0e94f00e1
terrycojones <terry@fluidinfo.com>
2014-01-09 02:01:23 +0000

    Pull all global BLAST settings from the first record when converting XML to JSON and output it.

--- dark/conversion.py
+++ dark/conversion.py
@@ -9,7 +9,11 @@ def convertBlastXMLToJSON(blastFilename, jsonFilename):
     out as serialized JSON to jsonFilename.
     """
     def _write(infp, outfp):
+        first = True
         for record in NCBIXML.parse(infp):
+            if first:
+                print >>outfp, dumps(convertBlastParamsToDict(record))
+                first = False
             print >>outfp, dumps(convertBlastRecordToDict(record))
 
     with open(blastFilename) as infp:
@@ -19,6 +23,73 @@ def convertBlastXMLToJSON(blastFilename, jsonFilename):
         else:
             # jsonFilename is already a file pointer (e.g., sys.stdout).
             _write(infp, jsonFilename)
+
+
+def convertBlastParamsToDict(record):
+    """
+    Pull the global BLAST parameters out of a BLAST record and return
+    them as a C{dict}.
+
+    Some of these attributes are useless (not filled in), but we record them
+    all just in case we one day need them or they start to be used or they
+    disappear etc. Any of those changes might alert us that something has
+    changed in BLAST XML output or in BioPython.
+
+    @param record: An instance of C{Bio.Blast.Record.Blast}. The attributes
+        on this don't seem to be documented. You'll need to look at the
+        BioPython source to see everything it contains.
+    """
+    result = {}
+    for attr in (
+            ## From Bio.Blast.Record.Header
+            'application',
+            'version',
+            'date',
+            'reference',
+            'query',
+            'query_letters',
+            'database',
+            'database_sequences',
+            'database_letters',
+            ## From Bio.Blast.Record.DatabaseReport
+            'database_name',
+            'posted_date',
+            'num_letters_in_database',
+            'num_sequences_in_database',
+            'ka_params',
+            'gapped',
+            'ka_params_gap',
+            ## From Bio.Blast.Record.Parameters
+            'matrix',
+            'gap_penalties',
+            'sc_match',
+            'sc_mismatch',
+            'num_hits',
+            'num_sequences',
+            'num_good_extends',
+            'num_seqs_better_e',
+            'hsps_no_gap',
+            'hsps_prelim_gapped',
+            'hsps_prelim_gapped_attemped',
+            'hsps_gapped',
+            'query_id',
+            'query_length',
+            'database_length',
+            'effective_hsp_length',
+            'effective_query_length',
+            'effective_database_length',
+            'effective_search_space',
+            'effective_search_space_used',
+            'frameshift',
+            'threshold',
+            'window_size',
+            'dropoff_1st_pass',
+            'gap_x_dropoff',
+            'gap_x_dropoff_final',
+            'gap_trigger',
+            'blast_cutoff'):
+        result[attr] = getattr(record, attr)
+    return result
 
 
 def convertBlastRecordToDict(record):
