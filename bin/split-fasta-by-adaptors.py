c88d60e8f2272d03eb03840868870cf61f7dc188
terrycojones <terry@fluidinfo.com>
2014-03-13 20:40:14 +0000

    A bunch of new tools for looking at and processing FASTA files.

---
+++ bin/split-fasta-by-adaptors.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+
+from Bio import SeqIO
+from collections import defaultdict
+import sys
+from dark.distance import levenshtein
+from math import log10, ceil
+
+
+def splitFASTAByAdaptor(knownAdaptors, adaptorLen, adaptorOffset,
+                        maximumDistance, outputPrefix, dryRun, verbose):
+    """
+    @param knownAdaptors: A C{set} of expected adaptor sequences.
+    @param adaptorOffset: The zero-based C{int} offset of the adaptor in
+        each sequence.
+    @param adaptorLen: The C{int} length of each adaptor sequence.
+    @param maximumDistance: The maximum distance an unknown adaptor will be
+        mapped to in an attempt to find its nearest known adaptor.
+    @param outputPrefix: A C{str} prefix that should be used in the file names
+        that are written out.
+    @param dryRun:  A C{bool}, if C{True} only print what would be done, don't
+        create any new FASTA files.
+    @param verbose: A C{bool}, if C{True} output additional information about
+        adaptor classes found and assigned.
+    """
+    adaptors = defaultdict(int)
+    discards = 0
+    classes = dict(zip(knownAdaptors, knownAdaptors))
+    reads = []
+
+    for count, seq in enumerate(SeqIO.parse(sys.stdin, 'fasta'), start=1):
+        reads.append(seq)
+        adaptor = str(seq.seq)[adaptorOffset:][:adaptorLen].upper()
+        adaptors[adaptor] += 1
+
+    order = sorted(adaptors, key=lambda adaptor: adaptors[adaptor],
+                   reverse=True)
+
+    for adaptor in order:
+        if adaptor in knownAdaptors:
+            if verbose:
+                print '%s: %s. Known adaptor' % (adaptor, adaptors[adaptor])
+        else:
+            distances = sorted((levenshtein(adaptor, known), known) for
+                               known in knownAdaptors)
+            # Discard the read if it's too far from its nearest neighbor or
+            # if it's nearest neighbor isn't unambiguous.
+            nearest = distances[0][0]
+            if nearest > maximumDistance or (len(knownAdaptors) > 1 and
+                                             nearest == distances[1][0]):
+                discards += 1
+                if verbose:
+                    print '%s: %s. Discarded, distances %r' % (
+                        adaptor, adaptors[adaptor], [d[0] for d in distances])
+            else:
+                correctedAdaptor = distances[0][1]
+                classes[adaptor] = correctedAdaptor
+                if verbose:
+                    print '%s: %s. Assigned to class %s, at dist %d' % (
+                        adaptor, adaptors[adaptor], correctedAdaptor,
+                        distances[0][0])
+
+    readGroups = defaultdict(list)
+
+    # Collect reads into classes.
+    for read in reads:
+        adaptor = str(read.seq)[adaptorOffset:][:adaptorLen].upper()
+        if adaptor in classes:
+            readGroups[classes[adaptor]].append(
+                read[adaptorOffset + adaptorLen:])
+
+    # Calculate the number of digits in the size of the biggest read group
+    # so we can nicely align the output.
+    width = int(ceil(log10(max(len(group) for group in readGroups.values()))))
+
+    # Write out the FASTA files for each adaptor class.
+    for adaptor, reads in readGroups.iteritems():
+        filename = outputPrefix + adaptor + '.fasta'
+        if dryRun:
+            print 'Would write %*d sequences for adaptor %s to %s' % (
+                width, len(reads), adaptor, filename)
+        else:
+            with open(filename, 'w') as fp:
+                SeqIO.write(reads, fp, 'fasta')
+            print 'Wrote %*d sequences for adaptor %s to %s' % (
+                width, len(reads), adaptor, filename)
+
+    discardPercent = float(discards) / count * 100.0
+    print '%d reads were found in total.' % count
+    print '%d of them (%.2f%%) were written to %d FASTA files.' % (
+        count - discards, 100.0 - discardPercent, len(readGroups))
+    print '%d reads (%.2f%%) were uncategorized and discarded.' % (
+        discards, discardPercent)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description=('From a set of known adaptors and a FASTA on stdin, '
+                     'write FASTA files split by adaptor'))
+
+    parser.add_argument(
+        'adaptors', type=str, nargs='+', metavar='adaptor',
+        help='the set of adaptors that were used in sequencing')
+
+    parser.add_argument(
+        '--adaptor-offset', metavar='N',
+        type=int, default=0, dest='adaptorOffset',
+        help='the offset of the adaptor in each sequence')
+
+    parser.add_argument(
+        '--maximum-distance', metavar='dist',
+        type=int, default=2, dest='maximumDistance',
+        help='The maximum distance an unknown adaptor will be '
+             'mapped to in an attempt to find its nearest known adaptor.')
+
+    parser.add_argument(
+        '--outputPrefix', metavar='prefix', type=str, default='MID-',
+        help='the prefix to use for newly created FASTA files')
+
+    parser.add_argument(
+        '--dry-run', type=bool, default=False, dest='dryRun',
+        help='If True, do not write new FASTA files, just show what would be '
+        'done')
+
+    parser.add_argument(
+        '--verbose', type=bool, default=False,
+        help='If True, print information about adaptor classes')
+
+    args = parser.parse_args()
+    adaptorLen = len(args.adaptors[0])
+    adaptors = set(args.adaptors)
+
+    # Check all adaptors are the same length.
+    if any((len(adaptor) != adaptorLen) for adaptor in adaptors):
+        print >>sys.stderr, 'All adaptors must be the same length.'
+        sys.exit(1)
+
+    splitFASTAByAdaptor(adaptors, adaptorLen, args.adaptorOffset,
+                        args.maximumDistance, args.outputPrefix, args.dryRun,
+                        args.verbose)
---
+++ test/test_sequence.py
@@ -0,0 +1,171 @@
+from unittest import TestCase
+from dark.sequence import findPrimer, findPrimerBidi, findPrimerBidiLimits
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC
+
+
+class TestFindPrimer(TestCase):
+    """
+    Tests for the dark.sequence.findPrimer function.
+    """
+
+    def testNotFound(self):
+        """
+        If a primer is not found, the empty list must be returned.
+        """
+        seq = Seq('ACGT', IUPAC.unambiguous_dna)
+        self.assertEqual([], findPrimer('BLAH', seq))
+
+    def testFoundAtStart(self):
+        """
+        If a primer is found at the start of a sequence, a list containing 0
+        must be returned.
+        """
+        seq = Seq('ACGT', IUPAC.unambiguous_dna)
+        self.assertEqual([0], findPrimer('AC', seq))
+
+    def testFoundAtEnd(self):
+        """
+        If a primer is found at the end of a sequence, the correct value
+        must be returned.
+        """
+        seq = Seq('ACGT', IUPAC.unambiguous_dna)
+        self.assertEqual([2], findPrimer('GT', seq))
+
+    def testFoundMultiple(self):
+        """
+        If a primer is found multiple times, the correct value
+        must be returned.
+        """
+        seq = Seq('ACGTACGT', IUPAC.unambiguous_dna)
+        self.assertEqual([0, 4], findPrimer('ACG', seq))
+
+    def testOverlapping(self):
+        """
+        If a primer is present twice but is overlapping, only the first
+        instance should be returned.
+        """
+        seq = Seq('GAAA', IUPAC.unambiguous_dna)
+        self.assertEqual([1], findPrimer('AA', seq))
+
+
+class TestFindPrimerBidi(TestCase):
+    """
+    Tests for the dark.sequence.findPrimerBidi function.
+    """
+
+    def testNotFound(self):
+        """
+        If a primer is not found, empty lists must be returned.
+        """
+        seq = Seq('ACGT', IUPAC.unambiguous_dna)
+        self.assertEqual(([], []), findPrimerBidi('BLAH', seq))
+
+    def testFoundStartEnd(self):
+        """
+        If a primer is found in both directions in a sequence (start of
+        the forward sequence, end of the reverse complement), the
+        correct value must be returned.
+        """
+        seq = Seq('ACGT', IUPAC.unambiguous_dna)
+        self.assertEqual(([0], [2]), findPrimerBidi('AC', seq))
+
+    def testFoundEndStart(self):
+        """
+        If a primer is found in both directions in a sequence (end of
+        the forward sequence, start of the reverse complement), the
+        correct value must be returned.
+        """
+        seq = Seq('ACGT', IUPAC.unambiguous_dna)
+        self.assertEqual(([2], [0]), findPrimerBidi('GT', seq))
+
+    def testFoundMultiple(self):
+        """
+        If a primer is found multiple times, the correct value
+        must be returned.
+        """
+        seq = Seq('ACGTACGT', IUPAC.unambiguous_dna)
+        self.assertEqual(([0, 4], [1, 5]), findPrimerBidi('ACG', seq))
+
+    def testOverlappingForwards(self):
+        """
+        If a primer is present twice forwards but is overlapping, only
+        the first instance should be returned.
+        """
+        seq = Seq('GAAA', IUPAC.unambiguous_dna)
+        self.assertEqual(([1], []), findPrimerBidi('AA', seq))
+
+    def testOverlappingBackwards(self):
+        """
+        If a primer is present twice backwards but is overlapping, only
+        the first instance should be returned.
+        """
+        seq = Seq('GTTT', IUPAC.unambiguous_dna)
+        self.assertEqual(([], [1]), findPrimerBidi('AA', seq))
+
+
+class TestFindPrimerBidiLimits(TestCase):
+    """
+    Tests for the dark.sequence.findPrimerBidiLimits function.
+    """
+
+    def testNotFound(self):
+        """
+        If a primer is not found, the returned offsets must include
+        the whole sequence.
+        """
+        seq = Seq('ACGT', IUPAC.unambiguous_dna)
+        self.assertEqual((0, 4), findPrimerBidiLimits('BLAH', seq))
+
+    def testFoundStartEnd(self):
+        """
+        If a primer is found in both directions in a sequence (start of
+        the forward sequence, end of the reverse complement), the
+        correct value must be returned.
+        """
+        seq = Seq('ACGT', IUPAC.unambiguous_dna)
+        self.assertEqual((2, 2), findPrimerBidiLimits('AC', seq))
+
+    def testFoundEndStart(self):
+        """
+        If a primer is found in both directions in a sequence (end of
+        the forward sequence, start of the reverse complement), the
+        correct value must be returned.
+        """
+        seq = Seq('ACGT', IUPAC.unambiguous_dna)
+        self.assertEqual((4, 4), findPrimerBidiLimits('GT', seq))
+
+    def testFoundMultiple(self):
+        """
+        If a primer is found multiple times, the correct value
+        must be returned.
+        """
+        seq = Seq('ACGTACGT', IUPAC.unambiguous_dna)
+        self.assertEqual((7, 8), findPrimerBidiLimits('ACG', seq))
+
+    def testOverlappingForwards(self):
+        """
+        If a primer is present twice forwards but is overlapping, only
+        the first instance should be returned.
+        """
+        seq = Seq('GAAA', IUPAC.unambiguous_dna)
+        self.assertEqual((3, 4), findPrimerBidiLimits('AA', seq))
+
+    def testOverlappingBackwards(self):
+        """
+        If a primer is present twice backwards but is overlapping, only
+        the first instance should be returned.
+        """
+        seq = Seq('GTTT', IUPAC.unambiguous_dna)
+        self.assertEqual((0, 1), findPrimerBidiLimits('AA', seq))
+
+    def testLonger(self):
+        """
+        Test a longer sequence.
+        """
+        seq = Seq('AAAAAAAAAA'
+                  'GGGGGGGGGG'
+                  'AAAAAAAAAA'
+                  'AAAAAAAAAA',
+                  IUPAC.unambiguous_dna)
+        self.assertEqual((20, 40), findPrimerBidiLimits('GGGGGGGGGG', seq))
