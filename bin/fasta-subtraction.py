e040e67d903225261d356cd10695383de6c563e0
Barbara Muehlemann <barbara.muhlemann@bluewin.ch>
2014-06-10 15:06:43 +0100

    changes

--- bin/fasta-subtraction.py
+++ bin/fasta-subtraction.py
@@ -1,28 +1,16 @@
 #!/usr/bin/env python
 
 import sys
-import argparse
 from Bio import SeqIO
-from dark import subtract
+from dark import fasta
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Generate a fastafile, containing a specified '
-        'set of reads',
-        epilog='Given a list of readIds and a fastaFile, generate '
-        'a new fastaFile based on these reads'
-    )
+    if len(sys.argv) == 1:
+        print >>sys.stderr, 'Usage: %s [1.fasta, 2.fasta, ...] < seq.fasta' % (
+            sys.argv[0])
+        sys.exit(1)
+    else:
+        reads = fasta.fastaSubtract(sys.argv[1:])
 
-    parser.add_argument(
-        '--fasta', metavar='FASTA-files', type=str,
-        help='a string of names of fastafiles which should be '
-        'subtracted, deliminited by commas.')
-
-    args = parser.parse_args()
-
-    files = args.fasta
-    fileList = files.split(', ')
-    reads = subtract.fastaSubtract(fileList)
-
-    SeqIO.write(reads, sys.stdout, 'fasta')
-    print >>sys.stderr, 'Found %d sequences.' % len(reads)
+        SeqIO.write(reads, sys.stdout, 'fasta')
+        print >>sys.stderr, 'Found %d sequences.' % len(reads)
