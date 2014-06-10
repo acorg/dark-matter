af0a96fa5619c35084aaef1d77e03270bf2aa724
Barbara Muehlemann <barbara.muhlemann@bluewin.ch>
2014-06-10 14:21:50 +0100

    changed test to remove 'invert'

--- test/test_subtract.py
+++ test/test_subtract.py
@@ -9,7 +9,7 @@ class TestFastaSubtract(TestCase):
     Test the fastaSubtract() function.
     """
 
-    def testTwoFastaFilesInvertFalse(self):
+    def testTwoFastaFiles(self):
         fasta1 = '>hey\nagtcagtcagtc\n>how\natgggtc\n>are\n\
             atggctattgaactgtatct'
         fasta3 = '>hey\nagtcagtcagtc\n>this\natgggtc\n>is\n\
@@ -22,24 +22,7 @@ class TestFastaSubtract(TestCase):
 
         self.assertEqual(result, testResult)
 
-    def testTwoFastaFilesInvertTrue(self):
-        fasta1 = '>hey\nagtcagtcagtc\n>you\nacctg\n>how\natgggtc\n>are\n\
-            atggctattgaactgtatct'
-        fasta3 = '>hey\nagtcagtcagtc\n>you\nacctg\n>this\natgggtc\n>is\n\
-            atggctattgaactgtatct'
-        testResult = []
-        testResult.append(SeqIO.read(StringIO('>how\natgggtc'), 'fasta'))
-        testResult.append(SeqIO.read(StringIO('>are\natggctattgaactgtatct'),
-                                     'fasta'))
-        testResult.append(SeqIO.read(StringIO('>this\natgggtc'), 'fasta'))
-        testResult.append(SeqIO.read(StringIO('>is\natggctattgaactgtatct'),
-                                     'fasta'))
-
-        result = subtract.fastaSubtract([StringIO(fasta1),
-                                        StringIO(fasta3)], invert=True)
-        self.assertEqual(result, testResult)
-
-    def testThreeFastaFilesInvertFalse(self):
+    def testThreeFastaFiles(self):
         fasta1 = '>hey\nagtcagtcagtc\n>you\nacctg\n>how\natgggtc\n>are\n\
             atggctattgaactgtatct'
         fasta3 = '>hey\nagtcagtcagtc\n>you\nacctg\n>this\natgggtc\n>is\n\
@@ -54,29 +37,6 @@ class TestFastaSubtract(TestCase):
         result = subtract.fastaSubtract([StringIO(fasta1),
                                         StringIO(fasta3),
                                         StringIO(fasta4)], invert=False)
-        self.assertEqual(result, testResult)
-
-    def testThreeFastaFilesInvertTrue(self):
-        fasta1 = '>hey\nagtcagtcagtc\n>you\nacctg\n>how\natgggtc\n>are\n\
-            atggctattgaactgtatct'
-        fasta3 = '>hey\nagtcagtcagtc\n>you\nacctg\n>this\natgggtc\n>is\n\
-            atggctattgaactgtatct'
-        fasta4 = '>hey\nagtcagtcagtc\n>you\nacctg\n>nothing\natgggtc\n>there\n\
-            atggctattgaactgtatct'
-        testResult = []
-        testResult.append(SeqIO.read(StringIO('>how\natgggtc'), 'fasta'))
-        testResult.append(SeqIO.read(StringIO('>are\natggctattgaactgtatct'),
-                                     'fasta'))
-        testResult.append(SeqIO.read(StringIO('>this\natgggtc'), 'fasta'))
-        testResult.append(SeqIO.read(StringIO('>is\natggctattgaactgtatct'),
-                                     'fasta'))
-        testResult.append(SeqIO.read(StringIO('>nothing\natgggtc'), 'fasta'))
-        testResult.append(SeqIO.read(StringIO('>there\natggctattgaactgtatct'),
-                                     'fasta'))
-
-        result = subtract.fastaSubtract([StringIO(fasta1),
-                                        StringIO(fasta3),
-                                        StringIO(fasta4)], invert=True)
         self.assertEqual(result, testResult)
 
     def testTwoFastaFilesNoCommons(self):
