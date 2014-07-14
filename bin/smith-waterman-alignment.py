06bcf38f59dd3261c13db1f264cbf26af98679fc
sophiemathias <sophie.mathias@outlook.com>
2014-07-14 18:20:35 +0100

    comment in bin file and other minor changes

--- bin/smith-waterman-alignment.py
+++ bin/smith-waterman-alignment.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 """
-
+Aligns each sequence in fileHandle1 to each sequence in fileHandle2. 
+Returns list of three strings for each alignment.
 """
 
 from dark.smith_waterman import smith_waterman
@@ -13,11 +14,11 @@ if len(sys.argv) != 3:
     sys.exit(1)
 
 else:
-    file_handle1 = sys.argv[1]
-    file_handle2 = sys.argv[2]
-    for record in SeqIO.parse(file_handle1, "fasta"):
+    fileHandle1 = sys.argv[1]
+    fileHandle2 = sys.argv[2]
+    for record in SeqIO.parse(fileHandle1, "fasta"):
         input1 = record.seq.upper()
-        for record in SeqIO.parse(file_handle2, "fasta"):
+        for record in SeqIO.parse(fileHandle2, "fasta"):
             input2 = record.seq.upper()
             result = smith_waterman(input1, input2)
             print "\n".join(result)
