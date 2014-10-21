b166cff7ab531872bbb59546d30636fac53e3e90
Barbara Muehlemann <barbara.muhlemann@bluewin.ch>
2014-10-21 09:57:00 +0100

    changed wording of ORF plots

--- dark/orfs.py
+++ dark/orfs.py
@@ -73,6 +73,6 @@ def addReversedORFs(fig, seq, minX, maxX, offsetAdjuster):
     fig.axis([minX, maxX, -1, 3])
     fig.set_yticks(np.arange(3))
     fig.set_ylabel('Frame', fontsize=17)
-    fig.set_title('Reversed target sequence start (%s) & stop (%s) codons' % (
-        ', '.join(sorted(START_CODONS)), ', '.join(sorted(STOP_CODONS))),
-        fontsize=20)
+    fig.set_title('Reversed target sequence start (%s) and stop (%s) codons' %
+                 (', '.join(sorted(START_CODONS)),
+                  ', '.join(sorted(STOP_CODONS))), fontsize=20)
