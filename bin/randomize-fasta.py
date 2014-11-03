91ace69aa624b40b7fe896680833ec5a15d1442d
terrycojones <terry@fluidinfo.com>
2014-11-03 12:34:57 +0000

    Added simple randomize-fasta.py script.

---
+++ bin/randomize-fasta.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""
+Read DNA FASTA from stdin and print FASTA to stdout, with the sequences
+randomized. The read ids and lengths are preserved.
+
+Note: This produces DNA sequences. If you have AA reads and you need this
+      functionality, we can add it.
+"""
+
+import sys
+from numpy.random import choice
+
+from dark.fasta import FastaReads
+from dark.reads import DNARead
+
+
+if __name__ == '__main__':
+    for read in FastaReads(sys.stdin):
+        seq = ''.join(choice(['A', 'C', 'G', 'T'], len(read), replace=True))
+        print DNARead(read.id, seq).toString('fasta')
