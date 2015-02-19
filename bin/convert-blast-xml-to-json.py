0cd589e70a229281160faaf4d292531a189b07ff
terrycojones <terry@fluidinfo.com>
2015-02-19 21:32:56 +0000

    Change to allow BLAST XML records reader to read from a file pointer. Changed convert-blast-xml-to-json.py to also be able to read and write from/to stdin/stdout.

--- bin/convert-blast-xml-to-json.py
+++ bin/convert-blast-xml-to-json.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 
+import sys
+from os.path import basename
+
+from dark.blast.conversion import XMLRecordsReader
+
 if __name__ == '__main__':
-    from dark.blast.conversion import XMLRecordsReader
-    import sys
-    if len(sys.argv) == 2:
+    nArgs = len(sys.argv)
+    if nArgs == 1:
+        reader = XMLRecordsReader(sys.stdin)
+        reader.saveAsJSON(sys.stdout)
+    elif nArgs == 2:
         reader = XMLRecordsReader(sys.argv[1])
         reader.saveAsJSON(sys.stdout)
-    elif len(sys.argv) == 3:
+    elif nArgs == 3:
         reader = XMLRecordsReader(sys.argv[1])
         with open(sys.argv[2], 'w') as fp:
             reader.saveAsJSON(fp)
     else:
-        print >>sys.stderr, 'Usage: %s infile.xml [outfile.json]' % sys.argv[0]
+        print >>sys.stderr, (
+            'Usage: %s [infile.xml [outfile.json]]' % basename(sys.argv[0]))
         sys.exit(1)
