c8490876c67c53940e4f3761f1a095ffa8f8c8cb
terrycojones <terry@jon.es>
2016-04-18 17:44:38 +0100

    Bumped version number.

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ scripts = [
 ]
 
 setup(name='dark-matter',
-      version='1.0.26',
+      version='1.0.27',
       packages=['dark'],
       include_package_data=True,
       url='https://github.com/acorg/dark-matter',
