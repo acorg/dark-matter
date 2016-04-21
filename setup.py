40d732afb4885185afe98197da0dc9e5609de8d7
terrycojones <terry@jon.es>
2016-04-21 10:33:05 +0100

    Added toDict and fromDict to all read classes. Bumped version number.

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ scripts = [
 ]
 
 setup(name='dark-matter',
-      version='1.0.28',
+      version='1.0.30',
       packages=['dark'],
       include_package_data=True,
       url='https://github.com/acorg/dark-matter',
