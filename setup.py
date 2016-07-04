75a33716ff13354b3a0b4fe8f74db20a36709d1a
Barbara Muehlemann <barbara.muhlemann@bluewin.ch>
2016-07-04 09:53:11 +0100

    Changed Readme.

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ scripts = [
 ]
 
 setup(name='dark-matter',
-      version='1.0.31',
+      version='1.0.32',
       packages=['dark'],
       include_package_data=True,
       url='https://github.com/acorg/dark-matter',
