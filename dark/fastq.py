1032ea401ed7b380b3f726b0726c383fe19dbda5
terrycojones <terry@jon.es>
2016-10-21 11:54:37 +0100

    Fixed docstring copy/paste error

--- dark/fastq.py
+++ dark/fastq.py
@@ -11,8 +11,8 @@ class FastqReads(Reads):
     Subclass of L{dark.reads.Reads} providing access to FASTQ reads.
 
     @param _files: Either a single C{str} file name or file handle, or a
-        C{list} of (possibly mixed) C{str} file names or file handles. Each
-        file / file handle must contain sequences in FASTQ format.
+        C{list} of C{str} file names and/or file handles. Each file or file
+        handle must contain sequences in FASTQ format.
     @param readClass: The class of read that should be yielded by iter.
     """
     def __init__(self, _files, readClass=DNARead):
