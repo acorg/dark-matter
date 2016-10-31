44aadfeee1364ecd7ebf8c96549917e77a07fc34
terrycojones <terry@jon.es>
2016-10-31 17:26:15 +0000

    Added gapped tests for frame=2 for DIAMOND normalized hsps

--- test/diamond/test_hsp.py
+++ test/diamond/test_hsp.py
@@ -1413,3 +1413,480 @@ class TestBlastxFramePlus1WithGaps(TestCase):
             'readStartInSubject': 1,
             'readEndInSubject': 5,
         }, normalized)
+
+
+class TestBlastxFramePlus2WithGaps(TestCase):
+    """
+    Tests for normalizeHSP for DIAMOND blastx output when frame=2 (i.e., the
+    query matches in the order it was given to DIAMOND, and the translation
+    frame starts at the second nucleotide) and with gaps.
+    """
+
+    # All query offsets and lengths must be in terms of nucleotides.
+    # Subject offsets are in terms of protein AA sequences.  This is how
+    # DIAMOND reports those offsets.
+    #
+    # In the little diagrams in the docstrings below, the first line is the
+    # subject and the second the query. Dots indicate where the matched
+    # region is. The queries are shown translated so as to line up properly
+    # with the subjects. Gaps are shown as a hyphen.
+
+    def testIdenticalWithQueryGap(self):
+        """
+        The subject start and end are identical to those of the translated
+        query, given one gap in the query.
+
+             ....
+             .-..
+        """
+        hsp = FakeHSP(subjectStart=1, subjectEnd=4, queryStart=2, queryEnd=10,
+                      frame=2, btop='1-K2')
+        normalized = normalizeHSP(hsp, 10, 'blastx')
+        self.assertEqual({
+            'subjectStart': 0,
+            'subjectEnd': 4,
+            'readStart': 0,
+            'readEnd': 3,
+            'readStartInSubject': 0,
+            'readEndInSubject': 4,
+        }, normalized)
+
+    def testIdenticalWithTwoQueryGaps(self):
+        """
+        The subject start and end are identical to those of the translated
+        query, given two gaps in the query.
+
+             ....
+             .--.
+        """
+        hsp = FakeHSP(subjectStart=1, subjectEnd=4, queryStart=2, queryEnd=7,
+                      frame=2, btop='1-K-K1')
+        normalized = normalizeHSP(hsp, 7, 'blastx')
+        self.assertEqual({
+            'subjectStart': 0,
+            'subjectEnd': 4,
+            'readStart': 0,
+            'readEnd': 2,
+            'readStartInSubject': 0,
+            'readEndInSubject': 4,
+        }, normalized)
+
+    def testIdenticalWithSubjectGap(self):
+        """
+        The subject start and end are identical to those of the translated
+        query, given one gap in the subject.
+
+             .-..
+             ....
+        """
+        hsp = FakeHSP(subjectStart=1, subjectEnd=3, queryStart=2, queryEnd=13,
+                      frame=2, btop='1K-2')
+        normalized = normalizeHSP(hsp, 13, 'blastx')
+        self.assertEqual({
+            'subjectStart': 0,
+            'subjectEnd': 3,
+            'readStart': 0,
+            'readEnd': 4,
+            'readStartInSubject': 0,
+            'readEndInSubject': 4,
+        }, normalized)
+
+    def testIdenticalWithTwoSubjectGaps(self):
+        """
+        The subject start and end are identical to those of the translated
+        query, given two gaps in the subject.
+
+             .--.
+             ....
+        """
+        hsp = FakeHSP(subjectStart=1, subjectEnd=2, queryStart=2, queryEnd=13,
+                      frame=2, btop='1K-K-1')
+        normalized = normalizeHSP(hsp, 13, 'blastx')
+        self.assertEqual({
+            'subjectStart': 0,
+            'subjectEnd': 2,
+            'readStart': 0,
+            'readEnd': 4,
+            'readStartInSubject': 0,
+            'readEndInSubject': 4,
+        }, normalized)
+
+    def testIdenticalWithQueryAndSubjectGap(self):
+        """
+        The subject start and end are identical to those of the translated
+        query, given one gap in the query and one in the subject.
+
+             .-..
+             ..-.
+        """
+        hsp = FakeHSP(subjectStart=1, subjectEnd=3, queryStart=2, queryEnd=10,
+                      frame=2, btop='1K--K2')
+        normalized = normalizeHSP(hsp, 10, 'blastx')
+        self.assertEqual({
+            'subjectStart': 0,
+            'subjectEnd': 3,
+            'readStart': 0,
+            'readEnd': 3,
+            'readStartInSubject': 0,
+            'readEndInSubject': 4,
+        }, normalized)
+
+    def testSubjectExtendsLeftWithQueryGap(self):
+        """
+        The subject overlaps the translated query to the left. The query has a
+        gap.
+
+              ss....
+                ..-.
+        """
+        hsp = FakeHSP(subjectStart=3, subjectEnd=6, queryStart=2, queryEnd=10,
+                      frame=2, btop='2-K1')
+        normalized = normalizeHSP(hsp, 10, 'blastx')
+        self.assertEqual({
+            'subjectStart': 2,
+            'subjectEnd': 6,
+            'readStart': 0,
+            'readEnd': 3,
+            'readStartInSubject': 2,
+            'readEndInSubject': 6,
+        }, normalized)
+
+    def testSubjectExtendsLeftWithSubjectGap(self):
+        """
+        The subject overlaps the translated query to the left. The subject has
+        a gap.
+
+              ss.-..
+                ....
+        """
+        hsp = FakeHSP(subjectStart=3, subjectEnd=5, queryStart=2, queryEnd=13,
+                      frame=2, btop='1K-2')
+        normalized = normalizeHSP(hsp, 13, 'blastx')
+        self.assertEqual({
+            'subjectStart': 2,
+            'subjectEnd': 5,
+            'readStart': 0,
+            'readEnd': 4,
+            'readStartInSubject': 2,
+            'readEndInSubject': 6,
+        }, normalized)
+
+    def testSubjectExtendsLeftWithQueryAndSubjectGap(self):
+        """
+        The subject overlaps the translated query to the left. The query and
+        subject both have a gap.
+
+              ss.-..
+                ..-.
+        """
+        hsp = FakeHSP(subjectStart=3, subjectEnd=5, queryStart=2, queryEnd=10,
+                      frame=2, btop='1K--K1')
+        normalized = normalizeHSP(hsp, 10, 'blastx')
+        self.assertEqual({
+            'subjectStart': 2,
+            'subjectEnd': 5,
+            'readStart': 0,
+            'readEnd': 3,
+            'readStartInSubject': 2,
+            'readEndInSubject': 6,
+        }, normalized)
+
+    def testQueryExtendsLeftWithQueryGap(self):
+        """
+        The translated query extends to the left of the subject and the query
+        has a gap.
+
+               ....
+             qq..-.
+        """
+        hsp = FakeHSP(subjectStart=1, subjectEnd=4, queryStart=8, queryEnd=16,
+                      frame=2, btop='2-K1')
+        normalized = normalizeHSP(hsp, 16, 'blastx')
+        self.assertEqual({
+            'subjectStart': 0,
+            'subjectEnd': 4,
+            'readStart': 2,
+            'readEnd': 5,
+            'readStartInSubject': -2,
+            'readEndInSubject': 4,
+        }, normalized)
+
+    def testQueryExtendsLeftWithSubjectGap(self):
+        """
+        The translated query extends to the left of the subject and the subject
+        has a gap.
+
+               ..-.
+             qq....
+        """
+        hsp = FakeHSP(subjectStart=1, subjectEnd=3, queryStart=8, queryEnd=19,
+                      frame=2, btop='2K-1')
+        normalized = normalizeHSP(hsp, 19, 'blastx')
+        self.assertEqual({
+            'subjectStart': 0,
+            'subjectEnd': 3,
+            'readStart': 2,
+            'readEnd': 6,
+            'readStartInSubject': -2,
+            'readEndInSubject': 4,
+        }, normalized)
+
+    def testQueryExtendsLeftWithQueryAndSubjectGap(self):
+        """
+        The translated query extends to the left of the subject and the query
+        and subject have gaps.
+
+               ..-.
+             qq.-..
+        """
+        hsp = FakeHSP(subjectStart=1, subjectEnd=3, queryStart=8, queryEnd=16,
+                      frame=2, btop='1-KK-1')
+        normalized = normalizeHSP(hsp, 16, 'blastx')
+        self.assertEqual({
+            'subjectStart': 0,
+            'subjectEnd': 3,
+            'readStart': 2,
+            'readEnd': 5,
+            'readStartInSubject': -2,
+            'readEndInSubject': 4,
+        }, normalized)
+
+    def testSubjectExtendsRightWithQueryGap(self):
+        """
+        The subject extends to the right of the translated query and the query
+        has a gap.
+
+               ....ss
+               ..-.
+        """
+        hsp = FakeHSP(subjectStart=1, subjectEnd=4, queryStart=2, queryEnd=10,
+                      frame=2, btop='2-K1')
+        normalized = normalizeHSP(hsp, 10, 'blastx')
+        self.assertEqual({
+            'subjectStart': 0,
+            'subjectEnd': 4,
+            'readStart': 0,
+            'readEnd': 3,
+            'readStartInSubject': 0,
+            'readEndInSubject': 4,
+        }, normalized)
+
+    def testSubjectExtendsRightWithSubjectGap(self):
+        """
+        The subject extends to the right of the translated query and the
+        subject has a gap.
+
+               ..-.ss
+               ....
+        """
+        hsp = FakeHSP(subjectStart=1, subjectEnd=3, queryStart=2, queryEnd=13,
+                      frame=2, btop='2K-1')
+        normalized = normalizeHSP(hsp, 13, 'blastx')
+        self.assertEqual({
+            'subjectStart': 0,
+            'subjectEnd': 3,
+            'readStart': 0,
+            'readEnd': 4,
+            'readStartInSubject': 0,
+            'readEndInSubject': 4,
+        }, normalized)
+
+    def testSubjectExtendsRightWithQueryAndSubjectGap(self):
+        """
+        The subject extends to the right of the translated query and the
+        query and subject both have gaps.
+
+               ..-.ss
+               .-..
+        """
+        hsp = FakeHSP(subjectStart=1, subjectEnd=3, queryStart=2, queryEnd=10,
+                      frame=2, btop='2-KK-1')
+        normalized = normalizeHSP(hsp, 10, 'blastx')
+        self.assertEqual({
+            'subjectStart': 0,
+            'subjectEnd': 3,
+            'readStart': 0,
+            'readEnd': 3,
+            'readStartInSubject': 0,
+            'readEndInSubject': 4,
+        }, normalized)
+
+    def testQueryExtendsRightWithQueryGap(self):
+        """
+        The translated query extends to the right of the subject and the
+        query has a gap.
+
+               ....
+               ..-.qq
+        """
+        hsp = FakeHSP(subjectStart=1, subjectEnd=4, queryStart=2, queryEnd=10,
+                      frame=2, btop='2-K1')
+        normalized = normalizeHSP(hsp, 16, 'blastx')
+        self.assertEqual({
+            'subjectStart': 0,
+            'subjectEnd': 4,
+            'readStart': 0,
+            'readEnd': 3,
+            'readStartInSubject': 0,
+            'readEndInSubject': 6,
+        }, normalized)
+
+    def testQueryExtendsRightWithSubjectGap(self):
+        """
+        The translated query extends to the right of the subject and the
+        subject has a gap.
+
+               ..-.
+               ....qq
+        """
+        hsp = FakeHSP(subjectStart=1, subjectEnd=3, queryStart=2, queryEnd=13,
+                      frame=2, btop='2K-1')
+        normalized = normalizeHSP(hsp, 19, 'blastx')
+        self.assertEqual({
+            'subjectStart': 0,
+            'subjectEnd': 3,
+            'readStart': 0,
+            'readEnd': 4,
+            'readStartInSubject': 0,
+            'readEndInSubject': 6,
+        }, normalized)
+
+    def testQueryExtendsRightWithQueryAndSubjectGap(self):
+        """
+        The translated query extends to the right of the subject and the
+        query and subject both have gaps.
+
+               ..-.
+               .-..qq
+        """
+        hsp = FakeHSP(subjectStart=1, subjectEnd=3, queryStart=2, queryEnd=10,
+                      frame=2, btop='1-KK-1')
+        normalized = normalizeHSP(hsp, 16, 'blastx')
+        self.assertEqual({
+            'subjectStart': 0,
+            'subjectEnd': 3,
+            'readStart': 0,
+            'readEnd': 3,
+            'readStartInSubject': 0,
+            'readEndInSubject': 6,
+        }, normalized)
+
+    def testQueryExtendsRightAndLeftWithQueryGap(self):
+        """
+        The translated query extends to the right and left of the subject and
+        the query has a gap.
+
+                ....
+              qq.-..q
+        """
+        hsp = FakeHSP(subjectStart=1, subjectEnd=4, queryStart=8, queryEnd=16,
+                      frame=2, btop='1-K2')
+        normalized = normalizeHSP(hsp, 19, 'blastx')
+        self.assertEqual({
+            'subjectStart': 0,
+            'subjectEnd': 4,
+            'readStart': 2,
+            'readEnd': 5,
+            'readStartInSubject': -2,
+            'readEndInSubject': 5,
+        }, normalized)
+
+    def testQueryExtendsRightAndLeftWithSubjectGap(self):
+        """
+        The translated query extends to the right and left of the subject and
+        the subject has a gap.
+
+                .-..
+              qq....q
+        """
+        hsp = FakeHSP(subjectStart=1, subjectEnd=3, queryStart=8, queryEnd=19,
+                      frame=2, btop='1K-2')
+        normalized = normalizeHSP(hsp, 22, 'blastx')
+        self.assertEqual({
+            'subjectStart': 0,
+            'subjectEnd': 3,
+            'readStart': 2,
+            'readEnd': 6,
+            'readStartInSubject': -2,
+            'readEndInSubject': 5,
+        }, normalized)
+
+    def testQueryExtendsRightAndLeftWithQueryAndSubjectGap(self):
+        """
+        The translated query extends to the right and left of the subject and
+        the query and the subject have gaps.
+
+                .-..
+              qq..-.q
+        """
+        hsp = FakeHSP(subjectStart=1, subjectEnd=3, queryStart=8, queryEnd=16,
+                      frame=2, btop='1K--K1')
+        normalized = normalizeHSP(hsp, 19, 'blastx')
+        self.assertEqual({
+            'subjectStart': 0,
+            'subjectEnd': 3,
+            'readStart': 2,
+            'readEnd': 5,
+            'readStartInSubject': -2,
+            'readEndInSubject': 5,
+        }, normalized)
+
+    def testSubjectExtendsRightAndLeftWithQueryGap(self):
+        """
+        The subject extends to the right and left of the translated query and
+        the query has a gap.
+
+               s....ss
+                .-..
+        """
+        hsp = FakeHSP(subjectStart=2, subjectEnd=5, queryStart=4, queryEnd=10,
+                      frame=2, btop='1-K1')
+        normalized = normalizeHSP(hsp, 10, 'blastx')
+        self.assertEqual({
+            'subjectStart': 1,
+            'subjectEnd': 5,
+            'readStart': 0,
+            'readEnd': 3,
+            'readStartInSubject': 1,
+            'readEndInSubject': 5,
+        }, normalized)
+
+    def testSubjectExtendsRightAndLeftWithSubjectGap(self):
+        """
+        The subject extends to the right and left of the translated query and
+        the subject has a gap.
+
+               s.-..ss
+                ....
+        """
+        hsp = FakeHSP(subjectStart=2, subjectEnd=4, queryStart=4, queryEnd=13,
+                      frame=2, btop='1K-1')
+        normalized = normalizeHSP(hsp, 13, 'blastx')
+        self.assertEqual({
+            'subjectStart': 1,
+            'subjectEnd': 4,
+            'readStart': 0,
+            'readEnd': 4,
+            'readStartInSubject': 1,
+            'readEndInSubject': 5,
+        }, normalized)
+
+    def testSubjectExtendsRightAndLeftWithQueryAndSubjectGap(self):
+        """
+        The subject extends to the right and left of the translated query and
+        the query and subjects have gaps.
+
+               s.-..ss
+                ..-.
+        """
+        hsp = FakeHSP(subjectStart=2, subjectEnd=4, queryStart=4, queryEnd=10,
+                      frame=2, btop='1K--K1')
+        normalized = normalizeHSP(hsp, 10, 'blastx')
+        self.assertEqual({
+            'subjectStart': 1,
+            'subjectEnd': 4,
+            'readStart': 0,
+            'readEnd': 3,
+            'readStartInSubject': 1,
+            'readEndInSubject': 5,
+        }, normalized)
