fc09b8a35698ace6cfd00ed66cbea96fdf15ad8b
terrycojones <terry@jon.es>
2016-11-19 14:27:00 +0000

    Lots of improvements to protein grouping and HTML output

--- dark/proteins.py
+++ dark/proteins.py
@@ -1,6 +1,10 @@
 import re
+from os.path import dirname, join
 from collections import defaultdict
 from operator import itemgetter
+from six.moves.urllib.parse import quote
+
+from dark.html import NCBISequenceLinkURL
 
 
 class ProteinGrouper(object):
@@ -10,14 +14,37 @@ class ProteinGrouper(object):
     @param sampleNameRegex: A C{str} that ......
     """
 
-    VIRUS_RE = re.compile('^([^\[]+)\[([^\]]+)\]$')
-    NO_VIRUS_TITLE = '<untitled>'
+    # The following regex is deliberately greedy (using .*) to consume the
+    # whole title before backtracking. That way it will match just the last
+    # [title] in a protein. This avoids situations in which two [...] delimited
+    # substrings are present in a protein name (in which case we just want the
+    # last). E.g., the following is a complete protein name:
+    #
+    #   gi|19919894|ref|NP_612577.1| Enzymatic polyprotein [Contains: Aspartic
+    #   protease; Endonuclease; Reverse transcriptase] [Carnation etched ring
+    #   virus]
+    #
+    # Unfortunately this doesn't match names like
+    #
+    #   gi|224808893|ref|YP_002643049.1| replication-associated protein [Tomato
+    #   leaf curl Nigeria virus-[Nigeria:2006]]
+    #
+    # but I decided not to worry about them (there are only 2 instances that I
+    # know of).
+    VIRUS_RE = re.compile('^(.*)\[([^\]]+)\]$')
+
+    # Do not use '<', '>' or any other HTML special chars in the following.
+    NO_VIRUS_TITLE = '[no virus name found in protein title]'
+
+    VIRALZONE = 'http://viralzone.expasy.org/cgi-bin/viralzone/search?query='
 
     def __init__(self, sampleNameRegex=None):
         self._sampleNameRegex = (re.compile(sampleNameRegex) if sampleNameRegex
                                  else None)
         # virusTitles is a dict of dicts of lists.
         self.virusTitles = defaultdict(lambda: defaultdict(list))
+        # sampleNames has values that give the sample panel's index.html file.
+        self.sampleNames = {}
 
     def addFile(self, filename, fp):
         """
@@ -36,6 +63,13 @@ class ProteinGrouper(object):
         else:
             sampleName = filename
 
+        # outDir is the assumed location of the directory where
+        # noninteractive-alignment-panel.py puts its HTML, image, and FASTA
+        # output.
+        outDir = join(dirname(filename), 'out')
+
+        self.sampleNames[sampleName] = join(outDir, 'index.html')
+
         for index, proteinLine in enumerate(fp):
             proteinLine = proteinLine[:-1]
             (coverage, medianScore, bestScore, readCount, hspCount,
@@ -50,13 +84,16 @@ class ProteinGrouper(object):
                 virusTitle = self.NO_VIRUS_TITLE
 
             self.virusTitles[virusTitle][sampleName].append({
+                'bluePlotFilename': join(outDir, '%d.png' % index),
                 'coverage': float(coverage),
+                'fastaFilename': join(outDir, '%d.fasta' % index),
                 'index': index,
                 'medianScore': float(medianScore),
                 'bestScore': float(bestScore),
                 'readCount': int(readCount),
                 'hspCount': int(hspCount),
                 'proteinTitle': proteinTitle,
+                'proteinURL': NCBISequenceLinkURL(proteinTitle),
             })
 
     def toStr(self):
@@ -66,6 +103,10 @@ class ProteinGrouper(object):
         readCountGetter = itemgetter('readCount')
         result = []
         append = result.append
+
+        append('%d viruses found in %d samples' %
+               (len(self.virusTitles), len(self.sampleNames)))
+        append('')
 
         for virusTitle in sorted(self.virusTitles):
             samples = self.virusTitles[virusTitle]
@@ -101,31 +142,82 @@ class ProteinGrouper(object):
             '<html>',
             '<head>',
             '<title>',
-            'Proteins grouped into viruses',
+            '%d viruses found in %d samples' % (len(self.virusTitles),
+                                                len(self.sampleNames)),
             '</title>',
             '</head>',
             '<body>',
             '<style>',
             '''
+            body {
+                margin-left: 2%;
+                margin-right: 2%;
+            }
             .sample {
-                /* color: green; */
+                margin-bottom: 2px;
             }
             .sample-name {
                 color: red;
             }
-            '''
+            .index {
+                font-size: small;
+            }
+            .protein-title {
+                font-family: "Courier New", Courier, monospace;
+            }
+            .stats {
+                font-family: "Courier New", Courier, monospace;
+                white-space: pre;
+            }
+            .protein-list {
+                margin-top: 2px;
+            }
+            ''',
             '</style>',
+            '</head>',
+            '<body>',
         ]
 
         append = result.append
 
-        for virusTitle in sorted(self.virusTitles):
+        virusTitles = sorted(self.virusTitles)
+        sampleNames = sorted(self.sampleNames)
+
+        append('<h1>%d viruses found in %d samples</h1>' %
+               (len(virusTitles), len(sampleNames)))
+
+        # Write a table of contents by virus.
+        append('<h2>Virus index</h2>')
+        append('<p class="index">')
+        for index, virusTitle in enumerate(virusTitles):
+            append('<a href="#virus-%d">%s</a>' % (index, virusTitle))
+            append('&middot;')
+        # Get rid of final middle dot.
+        result.pop()
+        append('</p>')
+
+        # Write a table of contents by sample.
+        append('<h2>Sample index</h2>')
+        append('<p class="index">')
+        for index, sampleName in enumerate(sampleNames):
+            append('<a href="#sample-%d">%s</a>' % (index, sampleName))
+            append('&middot;')
+        # Get rid of final middle dot.
+        result.pop()
+        append('</p>')
+
+        # Write all viruses (with samples (with proteins)).
+        append('<h1>Viruses by sample</h1>')
+        for index, virusTitle in enumerate(virusTitles):
             samples = self.virusTitles[virusTitle]
             sampleCount = len(samples)
             append(
-                '<h1 class="virus"><span class="virus-name">%s</span> '
-                '(in %d sample%s)</h1>' %
-                (virusTitle, sampleCount, '' if sampleCount == 1 else 's'))
+                '<a id="virus-%d">'
+                '<h2 class="virus"><span class="virus-title">%s</span> '
+                '(in %d sample%s, <a href="%s%s">viralzone</a>)</h2>' %
+                (index, virusTitle, sampleCount,
+                 '' if sampleCount == 1 else 's',
+                 self.VIRALZONE, quote(virusTitle)))
             for sampleName in sorted(samples):
                 proteins = samples[sampleName]
                 proteinCount = len(proteins)
@@ -133,23 +225,97 @@ class ProteinGrouper(object):
                 append(
                     '<p class=sample>'
                     '<span class="sample-name">%s</span> '
-                    '(%d protein%s, %d read%s)' %
+                    '(%d protein%s, %d read%s, <a href="%s">panel</a>)' %
                     (sampleName,
                      proteinCount, '' if proteinCount == 1 else 's',
-                     totalReads, '' if totalReads == 1 else 's'))
+                     totalReads, '' if totalReads == 1 else 's',
+                     self.sampleNames[sampleName]))
                 proteins.sort(key=titleGetter)
-                append('<ul class="proteins">')
+                append('<ul class="protein-list">')
                 for proteinMatch in proteins:
                     append(
                         '<li>'
-                        '%(coverage)f\t%(medianScore)f\t%(bestScore)f\t'
-                        '%(readCount)d\t%(hspCount)d\t%(index)d\t'
+                        '<span class="stats">'
+                        '%(coverage).2f %(medianScore).2f %(bestScore).2f '
+                        '%(readCount)3d %(hspCount)3d %(index)3d '
+                        '</span> '
+                        '<span class="protein-title">'
                         '%(proteinTitle)s'
-                        '</li>'
+                        '</span> '
+                        '(<a href="%(bluePlotFilename)s">blue plot</a>, '
+                        '<a href="%(fastaFilename)s">fasta</a>'
                         % proteinMatch)
+
+                    if proteinMatch['proteinURL']:
+                        # Append this directly to the last string in result, to
+                        # avoid introducing whitespace when we join result
+                        # using '\n'.
+                        result[-1] += (', <a href="%s">NCBI</a>' %
+                                       proteinMatch['proteinURL'])
+                    result[-1] += ')'
+
+                    append('</li>')
+
                 append('</ul>')
                 append('</p>')
-            append('')
+
+        # Write all samples (with viruses (with proteins)).
+        append('<h1>Samples by virus</h1>')
+        for index, sampleName in enumerate(sorted(sampleNames)):
+
+            sampleVirusTitles = set()
+            for virusTitle in virusTitles:
+                if sampleName in self.virusTitles[virusTitle]:
+                    sampleVirusTitles.add(virusTitle)
+
+            append(
+                '<a id="sample-%d">'
+                '<h2 class="sample"><span class="sample-name">%s</span> '
+                '(has proteins from %d virus%s, <a href="%s">panel</a>)</h2>' %
+                (index, sampleName, len(sampleVirusTitles),
+                 '' if len(sampleVirusTitles) == 1 else 'es',
+                 self.sampleNames[sampleName]))
+
+            for virusTitle in sorted(sampleVirusTitles):
+                proteins = self.virusTitles[virusTitle][sampleName]
+                proteinCount = len(proteins)
+                totalReads = sum(readCountGetter(p) for p in proteins)
+                append(
+                    '<p class="sample">'
+                    '<span class="virus-title">%s</span> '
+                    '(%d protein%s, %d read%s)' %
+                    (virusTitle,
+                     proteinCount, '' if proteinCount == 1 else 's',
+                     totalReads, '' if totalReads == 1 else 's'))
+                proteins.sort(key=titleGetter)
+                append('<ul class="protein-list">')
+                for proteinMatch in proteins:
+                    append(
+                        '<li>'
+                        '<span class="stats">'
+                        '%(coverage).2f %(medianScore).2f %(bestScore).2f '
+                        '%(readCount)3d %(hspCount)3d %(index)3d '
+                        '</span> '
+                        '<span class="protein-title">'
+                        '%(proteinTitle)s'
+                        '</span>'
+                        '(<a href="%(bluePlotFilename)s">blue plot</a>, '
+                        '<a href="%(fastaFilename)s">fasta</a>'
+                        % proteinMatch)
+
+                    if proteinMatch['proteinURL']:
+                        # Append this directly to the last string in result, to
+                        # avoid introducing whitespace when we join result
+                        # using '\n'.
+                        result[-1] += (', <a href="%s">NCBI</a>' %
+                                       proteinMatch['proteinURL'])
+                    result[-1] += ')'
+
+                    append('</li>')
+
+                append('</ul>')
+                append('</p>')
+
         append('</body>')
         append('</html>')
 
