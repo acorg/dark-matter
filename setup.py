ebb53ffa2c3ad3919452f760891f1fb96a330280
terrycojones <terry@jon.es>
2016-12-10 20:02:32 +0000

    TRIVIAL: fixed simple error in proteins.py

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ scripts = [
 ]
 
 setup(name='dark-matter',
-      version='1.0.86',
+      version='1.0.88',
       packages=['dark', 'dark.blast', 'dark.diamond'],
       include_package_data=True,
       url='https://github.com/acorg/dark-matter',
