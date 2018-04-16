19964470c00b3a74befb93cf1fca945121341e5e
terrycojones <terry@jon.es>
2018-04-16 22:05:22 +0100

    Travis CI setup tweaking.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env sh
 
+make dark/_gor4.so
+
 pip install -r requirements.txt
+
+# MySQL connector.
+tmp=/tmp/dm-install-$$
+mkdir $tmp
+cd $tmp
 curl -O -L http://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-2.1.3.tar.gz
 tar xzf mysql-connector-python-2.1.3.tar.gz
 cd mysql-connector-python-2.1.3
