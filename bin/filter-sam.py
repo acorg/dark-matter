df672658eb27a2a93a8f177c4628210907c4b11c
terrycojones <terry@jon.es>
2018-08-09 14:17:35 +0100

    More small changes, more tests.

--- bin/filter-sam.py
+++ bin/filter-sam.py
@@ -7,8 +7,8 @@ from pysam import AlignmentFile
 
 from dark.filter import (
     addFASTAFilteringCommandLineOptions, parseFASTAFilteringCommandLineOptions)
-from dark.reads import Read, Reads
-from dark.sam import samfile
+from dark.reads import Reads
+from dark.sam import samfile, SAMFilter
 
 
 if __name__ == '__main__':
@@ -18,10 +18,6 @@ if __name__ == '__main__':
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         description=('Given a SAM/BAM file and a set of filtering criteria '
                      'write filtered SAM/BAM to stdout.'))
-
-    parser.add_argument(
-        'samfile',
-        help='The SAM/BAM file to filter.')
 
     parser.add_argument(
         '--quiet', action='store_true', default=False,
@@ -35,102 +31,67 @@ if __name__ == '__main__':
         '--checkResultCount', type=int,
         help=('The number of alignments expected in the output. If this '
               'number is not seen, the script exits with status 1 (and an '
-              'error message is printed unless --quiet is used).'))
+              'error message is also printed, unless --quiet was used).'))
 
-    parser.add_argument(
-        '--dropUnmapped', default=False, action='store_true',
-        help='If given, unmapped matches will not be output.')
-
-    parser.add_argument(
-        '--dropSecondary', default=False, action='store_true',
-        help='If given, secondary matches will not be output.')
-
-    parser.add_argument(
-        '--dropSupplementary', default=False, action='store_true',
-        help='If given, supplementary matches will not be output.')
-
-    parser.add_argument(
-        '--dropDuplicates', default=False, action='store_true',
-        help=('If given, matches flagged as optical or PCR duplicates will '
-              'not be output.'))
-
-    parser.add_argument(
-        '--keepQCFailures', default=False, action='store_true',
-        help=('If given, reads that are considered quality control failures '
-              'will be included in the output.'))
-
-    parser.add_argument(
-        '--referenceWhitelist', metavar='NAME', action='append',
-        help=('A reference sequence id whose alignments should be output. '
-              'If omitted, alignments against all references will be output. '
-              'May be repeated.'))
-
-    parser.add_argument(
-        '--referenceBlacklist', metavar='NAME', action='append',
-        help=('A reference sequence id whose alignments should not be output. '
-              'If omitted, alignments against all references will be output. '
-              'May be repeated.'))
-
-    addFASTAFilteringCommandLineOptions(parser, filteringSAM=True)
+    addFASTAFilteringCommandLineOptions(parser)
+    SAMFilter.addFilteringOptions(parser)
 
     args = parser.parse_args()
+    reads = parseFASTAFilteringCommandLineOptions(args, Reads())
+    samFilter = SAMFilter.parseFilteringOptions(args, reads.filterRead,
+                                                storeQueryIds=True)
 
-    referenceWhitelist = (set(args.referenceWhitelist)
-                          if args.referenceWhitelist else None)
-    referenceBlacklist = (set(args.referenceBlacklist)
-                          if args.referenceBlacklist else None)
+    if samFilter.referenceIds:
+        # Make a header that only includes the wanted reference ids (if
+        # any).
+        print('If you get a segmentation violation, this is (I think) '
+              'due to the following pysam issue '
+              'https://github.com/pysam-developers/pysam/issues/716 It is '
+              'safer to run without using --referenceId.', file=sys.stderr)
+        with samfile(args.samfile) as sam:
+            header = sam.header.as_dict()
+        referenceIds = samFilter.referenceIds
+        sequences = [sequence for sequence in header['SQ']
+                     if sequence['SN'] in referenceIds]
+        referenceIdsFound = set(sequence['SN'] for sequence in sequences)
+        notFound = referenceIds - referenceIdsFound
+        if notFound:
+            if len(notFound) == len(referenceIds):
+                raise ValueError(
+                    'None of the provided --referenceId ids were found in %s' %
+                    args.samfile)
+            else:
+                # Just warn about the missing references.
+                print(
+                    'WARNING: reference%s (%s) given with --referenceId not '
+                    'found in %s.' % (
+                        '' if len(notFound) == 1 else 's',
+                        ', '.join(sorted(notFound)), args.samfile),
+                    file=sys.stderr)
 
-    if referenceWhitelist and referenceBlacklist and (
-            referenceWhitelist & referenceBlacklist):
-        raise ValueError(
-            'The reference whitelist and blacklist sets are not '
-            'disjoint. The intersection contains %s.' %
-            ', '.join(sorted(referenceWhitelist & referenceBlacklist)))
+        header['SQ'] = sequences
+        out = AlignmentFile(sys.stdout, mode='w' + args.bam, header=header)
+    else:
+        with samfile(args.samfile) as sam:
+            out = AlignmentFile(sys.stdout, mode='w' + args.bam, template=sam)
 
-    reads = parseFASTAFilteringCommandLineOptions(args, Reads(),
-                                                  filteringSAM=True)
-    filterRead = reads.filterRead
-    dropUnmapped = args.dropUnmapped
-    dropSecondary = args.dropSecondary
-    dropSupplementary = args.dropSupplementary
-    dropDuplicates = args.dropDuplicates
-    keepQCFailures = args.keepQCFailures
+    save = out.write
+    kept = 0
+    for kept, alignment in enumerate(samFilter.alignments(), start=1):
+        save(alignment)
 
-    kept = total = 0
-    with samfile(args.samfile) as samAlignment:
-        out = AlignmentFile(sys.stdout, mode='w' + args.bam,
-                            template=samAlignment)
-        save = out.write
-        for alignment in samAlignment.fetch():
-            total += 1
-
-            if (filterRead(Read(alignment.query_name,
-                                alignment.query_sequence,
-                                alignment.qual)) and
-                not (
-                    (alignment.is_unmapped and dropUnmapped) or
-                    (alignment.is_secondary and dropSecondary) or
-                    (alignment.is_supplementary and dropSupplementary) or
-                    (alignment.is_duplicate and dropDuplicates) or
-                    (alignment.is_qcfail and not keepQCFailures) or
-                    (referenceWhitelist is not None and
-                     alignment.reference_name not in referenceWhitelist) or
-                    (referenceBlacklist is not None and
-                     alignment.reference_name in referenceBlacklist))):
-                kept += 1
-                save(alignment)
-        out.close()
+    out.close()
 
     if not args.quiet:
+        total = samFilter.alignmentCount
         print('Read %d alignment%s, kept %d (%.2f%%).' %
               (total, '' if total == 1 else 's', kept,
                0.0 if total == 0 else kept / total * 100.0), file=sys.stderr)
 
-    if args.checkResultCount is not None:
-        if kept != args.checkResultCount:
-            if not args.quiet:
-                print('Did not write the expected %d alignment%s (wrote %d).' %
-                      (args.checkResultCount,
-                       '' if args.checkResultCount == 1 else 's', kept),
-                      file=sys.stderr)
-            sys.exit(1)
+    if args.checkResultCount is not None and kept != args.checkResultCount:
+        if not args.quiet:
+            print('Did not write the expected %d alignment%s (wrote %d).' %
+                  (args.checkResultCount,
+                   '' if args.checkResultCount == 1 else 's', kept),
+                  file=sys.stderr)
+        sys.exit(1)
--- setup.py
+++ setup.py
@@ -77,6 +77,7 @@ scripts = [
     'bin/read-blast-xml.py',
     'bin/sam-to-fasta-alignment.py',
     'bin/sam-reference-read-counts.py',
+    'bin/sam-references.py',
     'bin/sff-to-fastq.py',
     'bin/split-fasta-by-adaptors.py',
     'bin/summarize-fasta-bases.py',
