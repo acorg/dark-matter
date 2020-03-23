4c3115b2c5e585068988dfec0a7330b2d4f2ba37
terrycojones <terry@jon.es>
2020-03-23 21:52:18 +0100

    Added --md5 arg to fasta-sequences.py

--- bin/fasta-sequences.py
+++ bin/fasta-sequences.py
@@ -2,6 +2,8 @@
 
 from __future__ import print_function
 
+import hashlib
+
 from dark.reads import addFASTACommandLineOptions, parseFASTACommandLineOptions
 
 
@@ -12,9 +14,19 @@ if __name__ == '__main__':
         description=('Given FASTA on stdin, write the sequences '
                      'to stdout.'))
 
+    parser.add_argument(
+        '--md5', action='store_true', default=False,
+        help='Print MD5 checksums of seqeunces.')
+
     addFASTACommandLineOptions(parser)
     args = parser.parse_args()
     reads = parseFASTACommandLineOptions(args)
 
-    for read in reads:
-        print(read.sequence)
+    # Duplicate the loop to avoid a test on every read.
+    if args.md5:
+        md5 = hashlib.md5
+        for read in reads:
+            print(md5(read.sequence.encode('utf-8')).hexdigest())
+    else:
+        for read in reads:
+            print(read.sequence)
