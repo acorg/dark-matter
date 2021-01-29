fa1f974573107cdfa6b25626e79bdd595c51d56e
Udo Gieraths <udo.gieraths@gmail.com>
2021-01-29 10:46:45 +0100

    added missing space character

--- dark/bowtie2.py
+++ dark/bowtie2.py
@@ -251,7 +251,7 @@ class Bowtie2(object):
             "--input '%s' "
             "--output '%s' "
             "--sample-ploidy 1 "
-            "--dont-use-soft-clipped-bases true"
+            "--dont-use-soft-clipped-bases true "
             '-ERC GVCF' %
             (referenceFasta, inFile, vcfFile))
 
