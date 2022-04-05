a513be66df6ce3dbad2fe15a98b5540eadbdd194
terrycojones <terry@jon.es>
2022-04-05 17:36:05 +0200

    Added post facto cleanup of edlib alignments to removed unnecessary gaps.

--- test/test_aligners.py
+++ test/test_aligners.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 
-from dark.aligners import edlibAlign
+from random import choice, choices, sample
+
+from dark.aligners import edlibAlign, removeUnnecessaryGaps
 from dark.reads import DNARead, Reads
 
 
@@ -134,7 +136,7 @@ class TestEdlibAlign(TestCase):
 
         out1, out2 = list(edlibAlign(Reads([in1, in2])))
 
-        self.assertEqual(in1, out1)
+        self.assertEqual('ATGCCGTTCA', out1.sequence)
         self.assertEqual('ATGCC-TT--', out2.sequence)
 
     def testGapsAtBothEnds(self):
@@ -178,7 +180,59 @@ class TestEdlibAlign(TestCase):
         self.assertEqual('XXXAT-CC-TTCA---', out1.sequence)
         self.assertEqual('---ATGCCGTTCAYYY', out2.sequence)
 
-    def test30Knucleotides(self):
+    def testOneMiddleGapRemovalNotMinimized(self):
+        """
+        Test removal of one gap from the middle of each sequence when
+        unnecessary gaps are not removed.
+        """
+        in1 = DNARead('id1', 'TAACGCAGA')
+        in2 = DNARead('id2', 'TAAGTCAGA')
+
+        out1, out2 = list(edlibAlign(Reads([in1, in2]), minimizeGaps=False))
+
+        self.assertEqual('TAACG-CAGA', out1.sequence)
+        self.assertEqual('TAA-GTCAGA', out2.sequence)
+
+    def testOneMiddleGapRemovalMinimized(self):
+        """
+        Test removal of one gap from the middle of each sequence when
+        unnecessary gaps are removed.
+        """
+        in1 = DNARead('id1', 'TAACGCAGA')
+        in2 = DNARead('id2', 'TAAGTCAGA')
+
+        out1, out2 = list(edlibAlign(Reads([in1, in2])))
+
+        self.assertEqual(in1, out1)
+        self.assertEqual(in2, out2)
+
+    def testTwoMiddleGapsRemovalNotMinimized(self):
+        """
+        Test removal of two gaps from the middle of each sequence when the
+        post-processing step to remove unnecessary gaps is not done.
+        """
+        in1 = DNARead('id1', 'TAACGCAGAxTAACGCAGA')
+        in2 = DNARead('id2', 'TAAGTCAGAxTAAGTCAGA')
+
+        out1, out2 = list(edlibAlign(Reads([in1, in2]), minimizeGaps=False))
+
+        self.assertEqual('TAACG-CAGAxTAACG-CAGA', out1.sequence)
+        self.assertEqual('TAA-GTCAGAxTAA-GTCAGA', out2.sequence)
+
+    def testTwoMiddleGapsRemovalMinimized(self):
+        """
+        Test removal of two gaps from the middle of each sequence when
+        unnecessary gaps are removed.
+        """
+        in1 = DNARead('id1', 'TAACGCAGAxTAACGCAGA')
+        in2 = DNARead('id2', 'TAAGTCAGAxTAAGTCAGA')
+
+        out1, out2 = list(edlibAlign(Reads([in1, in2])))
+
+        self.assertEqual(in1, out1)
+        self.assertEqual(in2, out2)
+
+    def test30000Nucleotides(self):
         """
         Aligning two reads of length 30K nucleotides must work (and run
         quickly).
@@ -190,3 +244,153 @@ class TestEdlibAlign(TestCase):
 
         self.assertEqual(in1, out1)
         self.assertEqual(in2, out2)
+
+    def test500SubstitutionsIn30000Nucleotides(self):
+        """
+        Test that when 500 random substitutions are made to a random 30K
+        sequence, that the alignment is as expected (i.e., that the sequences
+        are pefectly aligned despite the mismatches).
+        """
+        n = 30_000
+        nSubs = 500
+        seq1 = choices('ACGT', k=n)
+        seq2 = seq1[:]
+        subOffsets = sample(range(n), nSubs)
+
+        # Make some changes in seq2.
+        for offset in subOffsets:
+            b4 = seq2[offset]
+            others = list(set('ACGT') - {seq2[offset]})
+            self.assertNotIn(b4, others)
+            seq2[offset] = choice(others)
+
+        seq1 = ''.join(seq1)
+        seq2 = ''.join(seq2)
+
+        in1 = DNARead('id1', seq1)
+        in2 = DNARead('id2', seq2)
+
+        out1, out2 = list(edlibAlign(Reads([in1, in2])))
+
+        self.assertEqual(len(in1), len(out1))
+        self.assertEqual(in1, out1)
+        self.assertEqual(seq2, out2.sequence)
+
+    def test500DeletionsIn30000Nucleotides(self):
+        """
+        Test that when 500 random deletions (though not next to each other) are
+        made to a random 30K sequence, that the alignment has gaps at the
+        expected sites.
+        """
+        n = 30_000
+        nDels = 500
+
+        # Make a first sequence that doesn't have repeated nucelotides.
+        # This makes sure edlib doesn't have an adjacent choice about where
+        # to put a gap in a string of identical nucelotides.
+        seq1 = []
+        while len(seq1) < n:
+            nt = choice('ACGT')
+            if not seq1 or seq1[-1] != nt:
+                seq1.append(nt)
+
+        # Pick some deletion sites, but not next to one another (otherwise
+        # it is too hard to know what gaps to test for below).
+        delOffsets = set()
+        while len(delOffsets) < nDels:
+            offset = choice(range(n))
+            if not (delOffsets & {offset - 1, offset, offset + 1}):
+                delOffsets.add(offset)
+
+        # seq2 will be a copy of seq1, but sans the deleted sites.
+        seq2 = []
+        for offset in range(n):
+            if offset not in delOffsets:
+                seq2.append(seq1[offset])
+
+        seq1 = ''.join(seq1)
+        seq2 = ''.join(seq2)
+
+        in1 = DNARead('id1', seq1)
+        in2 = DNARead('id2', seq2)
+
+        out1, out2 = list(edlibAlign(Reads([in1, in2])))
+
+        # Check that all the sites that were deleted in seq2 now have a gap.
+        for offset in range(n):
+            if offset in delOffsets:
+                # There is (maybe) a small chance the assertion below will
+                # fail, in which case the following code can be uncommented
+                # to see what is going on. An info line will be printed,
+                # with a '*' at offsets where there should be a gap but
+                # isn't and a '|' at gap offsets. This is followed by the
+                # two aligned sequences, with '-' characters. I'm leaving
+                # this code here for now, until we are very sure it doesn't
+                # fail due to some pathological selection of substitution
+                # sites and nucelotides. If that does happen, reducing
+                # nDels or broadening the exclusion zone around each
+                # deletion (to more than +/- 1 nt) should greatly reduce
+                # the probability of this test failing.
+                #
+                # if out2.sequence[offset] != '-':
+                #     for a in range(n):
+                #         print('*' if a == offset else (
+                #             '|' if a in delOffsets else ' '), end='')
+                #     print()
+                #     print(out1.sequence)
+                #     print(out2.sequence)
+                self.assertEqual('-', out2.sequence[offset])
+
+
+class TestRemoveUnnecessaryGaps(TestCase):
+    """
+    Test the removeUnnecessaryGaps function.
+    """
+    def testEmptyStrings(self):
+        """
+        No gaps can be removed from two empty strings.
+        """
+        self.assertEqual(('', ''), removeUnnecessaryGaps('', ''))
+
+    def testStringsWithNoGaps(self):
+        """
+        No gaps can be removed from two strings with no gaps.
+        """
+        self.assertEqual(('AG', 'AT'), removeUnnecessaryGaps('AG', 'AT'))
+
+    def testOneGapOneIdenticalChar(self):
+        """
+        Identical strings of length two with one gap have the gap removed.
+        """
+        self.assertEqual(('A', 'A'), removeUnnecessaryGaps('A-', '-A'))
+        self.assertEqual(('A', 'A'), removeUnnecessaryGaps('-A', 'A-'))
+
+    def testOneGapOneNonIdenticalChar(self):
+        """
+        Non-identical strings of length two with one gap have the gap removed.
+        """
+        self.assertEqual(('A', 'B'), removeUnnecessaryGaps('-A', 'B-'))
+        self.assertEqual(('A', 'B'), removeUnnecessaryGaps('A-', '-B'))
+
+    def testFirstGapInFirstString(self):
+        """
+        A gap can be removed from two strings when the first string is the
+        first to have a gap.
+        """
+        self.assertEqual(('TAAGTCAGA', 'TAACGCAGA'),
+                         removeUnnecessaryGaps('TAA-GTCAGA', 'TAACG-CAGA'))
+
+    def testFirstGapInSecondString(self):
+        """
+        A gap can be removed from two strings when the second string is the
+        first to have a gap.
+        """
+        self.assertEqual(('TAACGCAGA', 'TAAGTCAGA'),
+                         removeUnnecessaryGaps('TAACG-CAGA', 'TAA-GTCAGA'))
+
+    def testConsecutiveGaps(self):
+        """
+        Two consecutive gaps can be removed.
+        """
+        self.assertEqual(('TAAGTCAGA', 'TAACGCAGA'),
+                         removeUnnecessaryGaps('TAA--GTCAGA', 'TAACG--CAGA'))
