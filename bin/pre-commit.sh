553d6e640849e94371b24e1db9f5d389e27c8829
terrycojones <tcj25@cam.ac.uk>
2025-05-07 01:55:19 +0200

    Cleaned Makefile. Put pointer to author names into LICENSE.

--- bin/pre-commit.sh
+++ bin/pre-commit.sh
@@ -7,6 +7,12 @@
 #   $ cd .git/hooks
 #   $ ln -s ../../bin/pre-commit.sh pre-commit
 
+# Add our virtualenv bin to PATH. Git commit seems to muck with PATH :-(
+if [ -n "$VIRTUAL_ENV" ]
+then
+    PATH="$VIRTUAL_ENV/bin:$PATH"
+fi
+
 ruff check --silent
 
 if [ $? -ne 0 ]
