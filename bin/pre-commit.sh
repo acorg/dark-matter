10035cd8965b903945541559bd4a0c9d3c729ef3
terrycojones <tcj25@cam.ac.uk>
2025-05-08 14:54:46 +0200

    Modified git pre-commit hook again following review discussion with @imLew.

--- bin/pre-commit.sh
+++ bin/pre-commit.sh
@@ -7,19 +7,31 @@
 #   $ cd .git/hooks
 #   $ ln -s ../../bin/pre-commit.sh pre-commit
 
+tmp=$(mktemp)
+
+# This assumes we don't have files with spaces in their names.
+FILES=$(git diff-index --cached --name-only HEAD | egrep '\.py$')
+
 if command -v uv >/dev/null
 then
-    uv run ruff check --silent
+    uv run ruff check --fix --extend-select I $FILES > $tmp 2>&1
 elif command -v ruff >/dev/null
 then
-    ruff check --silent
+    ruff check --fix --extend-select I $FILES > $tmp 2>&1
+else
+    # ruff cannot be found.
+    :
 fi
 
 if [ $? -ne 0 ]
 then
     echo 'COMMIT FAILED: ruff check did not run cleanly:' >&2
+    cat $tmp >&2
+    rm $tmp
     exit 1
 fi
+
+rm $tmp
 
 make pytest
 
