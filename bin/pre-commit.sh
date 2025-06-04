a589e62d5c3ab53e55509af692b9d6a229dea1fb
terrycojones <tcj25@cam.ac.uk>
2025-06-04 20:41:27 +0200

    Make pre-commit.sh ignore deleted files when doing the ruff check.

--- bin/pre-commit.sh
+++ bin/pre-commit.sh
@@ -9,12 +9,13 @@
 
 tmp=$(mktemp)
 
-# This assumes we don't have files with spaces in their names.
+# This assumes we don't have files with spaces in their names. The files
+# we'll ask ruff to check must exclude ones that have been deleted (these
+# have a leading D in the output of the git diff-index command).
 FILES=$(git diff-index --cached --name-status HEAD | egrep -v '^D' | egrep '\.py$ | cut -f2')
 
 if command -v uv >/dev/null
 then
-    echo "HERE" $(type uv) >&2
     uv run ruff check --fix --extend-select I $FILES > $tmp 2>&1
 elif command -v ruff >/dev/null
 then
